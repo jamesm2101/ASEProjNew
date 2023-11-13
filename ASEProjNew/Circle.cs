f5f422700abfe01543d706661fe0ddfe2dabdbd7
James <millar687760@gmail.com>
2023-11-13 02:08:38 +0000

    Circle class created an inherits the x and y values from Shape. These are used along with radius to draw the circle

---
+++ ASEProjNew/Circle.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+namespace ASEProjNew
+{
+    public class Circle : Shape
+    {
+        int radius;
+
+        public Circle(int x, int y, int radius) : base(x, y)
+        {
+            this.radius = radius;
+        }
+
+        public override void draw(Graphics g)
+        {
+            Pen pen = new Pen(Color.Red, 2);
+            SolidBrush brush = new SolidBrush(Color.Black);
+            g.DrawEllipse(pen, x, y, radius, radius);
+            g.FillEllipse(brush, new Rectangle(x, y, radius, radius));
+
+        }
+    }
+}
