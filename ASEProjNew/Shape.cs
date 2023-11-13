fb12a91647db6ed59355a631f942cd5aadb5edff
James <millar687760@gmail.com>
2023-11-13 01:59:20 +0000

    Shape class created with integer values being created to help draw

---
+++ ASEProjNew/Shape.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ASEProjNew
+{
+    public abstract class Shape
+    {
+        protected int x, y;
+
+        public Shape(int x, int y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+        public abstract void draw(Graphics g);
+    }
+}
