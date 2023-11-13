a5cdf8222f5d6c5b20a1d7a24d87735b78931a03
James <millar687760@gmail.com>
2023-11-13 04:22:51 +0000

    RecogniseWhenTextBoxDoesNotIncludeCommand test + XML

--- ASEUnitTest/UnitTest1.cs
+++ ASEUnitTest/UnitTest1.cs
@@ -1,20 +1,36 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using ASEProjNew;
+using System.Windows.Forms;
 
 namespace ASEUnitTest
 {
+    /// <summary>
+    /// Test Class to hold all tests
+    /// </summary>
     [TestClass]
     public class UnitTest1
     {
+        /// <summary>
+        /// Recognises when the textbox does not include a selected command
+        /// </summary>
         [TestMethod]
-        public void Run_WhenCompleteCommandBoxIsInvalid_ShouldThrowException()
+        public void RecogniseWhenTextBoxDoesNotIncludeCommand()
         {
             //Arrange
-            Form1 form1 = new Form1();
-            string completelines = "";
-         
+            TextBox textBox = new TextBox();
+            string input = "";
+
             //Act and assert
-            Assert.ThrowsException<ArgumentException>(() => { });
+            bool containsCommand = textBox.ContainsCommand(input, "moveTo");
+            Assert.IsFalse(containsCommand);
+        }
+    }
+
+    public class TextBox
+    {
+        public bool ContainsCommand(string input, string command)
+        {
+            return input.Contains(command);
         }
     }
 }
